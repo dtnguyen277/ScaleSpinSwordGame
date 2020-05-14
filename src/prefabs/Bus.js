6fc05475358562c9556cb1bfe74c5b5eeedd2978
dtnguyen277 <dtnguyen277@users.noreply.github.com>
2020-05-14 11:47:43 -0700

    test

--- src/prefabs/Bus.js
+++ src/prefabs/Bus.js
@@ -2,7 +2,7 @@ class Bus extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
-
+        //test
     }
 
     update() {
