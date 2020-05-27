ec5758df60646187122a01a864deae76beaa087a
dtnguyen277 <dtnguyen277@users.noreply.github.com>
2020-05-27 02:19:38 -0700

    finished bridge mode

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -9,7 +9,8 @@ class Play extends Phaser.Scene {
         this.MIN_SWORD_SCALE = 0.25
         this.MAX_SWORD_SCALE = 3;
         var touchingGround = false;
-        this.bridgeMode = false;
+        this.gameMode = 0;
+        this.oneToggle = true;
     }
 
     // load assets here: Bus, Infinite Scrolling road on Y axis
@@ -32,12 +33,14 @@ class Play extends Phaser.Scene {
 
         this.p1 = this.matter.add.sprite(game.config.width/2, game.config.height/2, "kenney_sheet", 402);
         Phaser.Physics.Matter.Matter.Body.setInertia(this.p1.body, Infinity);
+        this.matter.add.mouseSpring();
 
         this.matter.add.rectangle(game.config.width/2, game.config.height-8, game.config.width, 10, {isStatic: true});
         this.matter.add.rectangle(game.config.width-8, game.config.height/2, 10, game.config.height, {isStatic: true});
         this.matter.add.rectangle(8, game.config.height/2, 10, game.config.height, {isStatic: true});
         this.matter.add.rectangle(game.config.width/2, game.config.height-150, 100, 10, {isStatic: true});
-        this.matter.add.mouseSpring();
+        this.swordBridge = this.matter.add.sprite(-200, 0 - 100, 'sword', null, {isStatic: true, ignoreGravity: true});
+        this.swordBridge.angle = 90;
         // this.matter.add.joint(this.p1, this.sword, 50, 1, {ignoreGravity: true, density: 1});
 
         this.matter.world.on("collisionactive", (p1, ground) => {
@@ -52,52 +55,76 @@ class Play extends Phaser.Scene {
         this.moveLeft = this.input.keyboard.addKey('A');
         this.jump = this.input.keyboard.addKey('SPACE');
         this.moveTowardsSword = this.input.keyboard.addKey('S');
-        this.toggleBridgeMode = this.input.keyboard.addKey('N');
+        this.gameModeToggle = this.input.keyboard.addKey('N');
+        this.p1Pos = new Phaser.Math.Vector2();
 
     }
 
     update() {
         // attach sword to player
-        this.sword.x = this.p1.x;
-        this.sword.y = this.p1.y;
-        
-        //directional controls 
-        if (this.moveLeft.isDown) {
-            this.p1.setVelocityX(-this.ACCELERATION);
-        }
-        else if (this.moveRight.isDown) {
-            this.p1.setVelocityX(this.ACCELERATION);
-        }
-        //jump control
-        if (Phaser.Input.Keyboard.JustDown(this.jump) && this.touchingGround) {
-            this.p1.setVelocityY(-this.ACCELERATION*6);
-            this.sound.play('jump');
-            this.touchingGround = false;
+        this.sword.x = this.p1Pos.x;
+        this.sword.y = this.p1Pos.y;
+        console.log(this.gameMode%3);
+        if (this.gameMode%3 != 1) {
+            //directional controls 
+            if (this.moveLeft.isDown) {
+                this.p1.setVelocityX(-this.ACCELERATION);
+            }
+            else if (this.moveRight.isDown) {
+                this.p1.setVelocityX(this.ACCELERATION);
+            }
+            //jump control
+            if (Phaser.Input.Keyboard.JustDown(this.jump) && this.touchingGround) {
+                this.p1.setVelocityY(-this.ACCELERATION*6);
+                this.sound.play('jump');
+                this.touchingGround = false;
+            }
+            if (this.gameMode%3 == 0) {
+                this.p1Pos.set(this.p1.x, this.p1.y);
+                // make sword angle follow cursor
+                this.angleToPointer = Phaser.Math.Angle.Between(this.sword.x, this.sword.y, 
+                this.input.activePointer.worldX, this.input.activePointer.worldY);
+                this.sword.rotation = Phaser.Math.Angle.RotateTo(
+                    this.sword.rotation,
+                    this.angleToPointer + Math.PI/2,
+                    this.SWORD_TURN_SPEED
+                );
+            }
         }
 
         // sword scale controls
         // WIP
-        if (this.scaleDown.isDown && this.sword.scaleY >= this.MIN_SWORD_SCALE) {
-            // Phaser.Physics.Matter.Matter.Body.scale(this.sword.body, 1.01, 1);
+        if (this.scaleDown.isDown) {
+            // Phaser.Physics.Matter.Matter.Body.scale(this.swordBridge.body, 1.01, 1);
+            this.swordBridge.scaleX += .1;
         }
-        if (this.scaleUp.isDown && this.sword.scaleY <= this.MAX_SWORD_SCALE) {
-            // Phaser.Physics.Matter.Matter.Body.scale(this.sword.body, .99, 1);
-        }
-
-        //bridge mode
-        if (Phaser.Input.Keyboard.JustDown(this.toggleBridgeMode)) { this.bridgeMode = !this.bridgeMode; }
-        if (this.bridgeMode) {
-            
+        if (this.scaleUp.isDown) {
+            // Phaser.Physics.Matter.Matter.Body.scale(this.swordBridge.body, .99, 1);
+            this.swordBridge.scaleX -= .1;
         }
 
-        // make sword angle follow cursor
-        this.angleToPointer = Phaser.Math.Angle.Between(this.sword.x, this.sword.y, 
-        this.input.activePointer.worldX, this.input.activePointer.worldY);
-        this.sword.rotation = Phaser.Math.Angle.RotateTo(
-            this.sword.rotation,
-            this.angleToPointer + Math.PI/2,
-            this.SWORD_TURN_SPEED
-          );
+        //change game mode mode
+        if (Phaser.Input.Keyboard.JustDown(this.gameModeToggle)) { this.gameMode++; }
+        // gamemode 0 is default
+        // gamemode 1 is bridge mode scale and change position of sword
+        // gamemode 2 is walk around with no swinging sword
+        if (this.gameMode % 3 == 1 && this.oneToggle) {
+            this.swordBridge.x = this.p1.x;
+            this.swordBridge.y = this.p1.y -100;
+            this.p1Pos.set(-100, -100);
+            this.swordBridge.setStatic(0);
+            this.oneToggle = false;
+        }
+        else if (this.gameMode % 3 == 2 && this.oneToggle == false) {
+            this.swordBridge.setStatic(1);
+            this.oneToggle = true;
+        }
+        else if (this.gameMode%3 == 0) {
+            this.swordBridge.x = -200;
+            this.swordBridge.y = 0;
+            this.swordBridge.scaleX = 1;
+        }
+
     }
 
 
