2dfbd44eb6e63bbc74e1751eb95dcb5c01ee9a68
rakhin <37986176+rakhin@users.noreply.github.com>
2020-05-31 13:08:49 -0700

    Add files via upload
    Implemented the camera zoom in Play.js and fixed the walking animation

---
+++ ScaleSpinSwordGame-master/src/main.js
@@ -0,0 +1,39 @@
+// Daniel Nguyen
+// Dnguy182@ucsc.edu
+// Rakhi Narain
+// rmnarain@ucsc.edu
+// Game Title: 
+// Date of Completion: 
+// Creative Tilt
+/*
+
+*/
+
+let config = {
+     type: Phaser.CANVAS,
+     pixelArt: true,
+     width: 1280,
+     height: 720,
+     physics: {
+        default: "matter",
+        matter: {
+            debug: true,
+        }
+    },
+     scene: [ Menu, Play ],
+};
+
+let game = new Phaser.Game(config);
+
+// define game settings
+game.settings = {
+    
+};
+
+// keys that we will use
+let keyLEFT, keyRIGHT;
+let cursors = null;
+
+// global highscore value
+let hiScore = 0;
+
